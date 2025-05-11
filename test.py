b4efecfb900f269626eb4603934c2097535890a9
tecno <t0pnut@yandex.com>
2025-05-11 21:02:44 +0300

    fourth

--- test.py
+++ test.py
@@ -7,5 +7,11 @@ print(requests.get('http://127.0.0.1:8080/api/jobs/99').json())
 print(requests.get('http://127.0.0.1:8080/api/jobs/str').json())
 
 
-print(requests.post('http://127.0.0.1:8080/api/jobs', json={'name': 'охранник'}).json())
+print(requests.post('http://127.0.0.1:8080/api/jobs', json={'name': 'вожатый'}).json())
+# пустое тело
+print(requests.post('http://127.0.0.1:8080/api/jobs', json={}).json())
+# нет name
+print(requests.post('http://127.0.0.1:8080/api/jobs', json={'health': '80'}).json())
+
+
 print(requests.get('http://127.0.0.1:8080/api/jobs').json())
