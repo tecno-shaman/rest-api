6b82816a1caa0350f678d971d0b1c9d83c7f714b
tecno <t0pnut@yandex.com>
2025-05-11 20:52:29 +0300

    third

--- data/jobs_api.py
+++ data/jobs_api.py
@@ -33,24 +33,18 @@ def get_one_news(jobs_id):
         }
     )
 
-# @blueprint.route('/api/news', methods=['POST'])
-# def create_news():
-#     if not request.json:
-#         return make_response(jsonify({'error': 'Empty request'}), 400)
-#     elif not all(key in request.json for key in
-#                  ['title', 'content', 'user_id', 'is_private']):
-#         return make_response(jsonify({'error': 'Bad request'}), 400)
-#     db_sess = db_session.create_session()
-#     news = Works(
-#         title=request.json['title'],
-#         content=request.json['content'],
-#         user_id=request.json['user_id'],
-#         is_private=request.json['is_private']
-#     )
-#     db_sess.add(news)
-#     db_sess.commit()
-#     return jsonify({'id': news.id})
-#
+@blueprint.route('/api/jobs', methods=['POST'])
+def add_job():
+    if not request.json:
+        return make_response(jsonify({'error': 'Empty request'}), 400)
+    elif not 'name' in request.json:
+        return make_response(jsonify({'error': 'Bad request'}), 400)
+    db_sess = db_session.create_session()
+    job = Works(name = request.json['name'])
+    db_sess.add(job)
+    db_sess.commit()
+    return jsonify({'id': job.id})
+
 # @blueprint.route('/api/news/<int:news_id>', methods=['DELETE'])
 # def delete_news(news_id):
 #     db_sess = db_session.create_session()
--- test.py
+++ test.py
@@ -2,3 +2,10 @@ import requests
 
 print(requests.get('http://127.0.0.1:8080/api/jobs').json())
 print(requests.get('http://127.0.0.1:8080/api/jobs/0').json())
+
+print(requests.get('http://127.0.0.1:8080/api/jobs/99').json())
+print(requests.get('http://127.0.0.1:8080/api/jobs/str').json())
+
+
+print(requests.post('http://127.0.0.1:8080/api/jobs', json={'name': 'охранник'}).json())
+print(requests.get('http://127.0.0.1:8080/api/jobs').json())
