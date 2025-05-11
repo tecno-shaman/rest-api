a966b4c8b8b1918f3e3e8eb0489652cd911a6e8d
tecno <t0pnut@yandex.com>
2025-05-11 21:12:03 +0300

    6

--- del.py
+++ del.py
@@ -2,5 +2,11 @@ import requests
 
 print(requests.get('http://127.0.0.1:8080/api/jobs').json())
 
-print(requests.delete('http://127.0.0.1:8080/api/jobs/1').json())
+print(requests.delete('http://127.0.0.1:8080/api/jobs/2').json())
+
+print(requests.get('http://127.0.0.1:8080/api/jobs').json())
+
+print(requests.delete('http://127.0.0.1:8080/api/jobs/99').json())
+print(requests.delete('http://127.0.0.1:8080/api/jobs/водитель').json())
+
 print(requests.get('http://127.0.0.1:8080/api/jobs').json())
